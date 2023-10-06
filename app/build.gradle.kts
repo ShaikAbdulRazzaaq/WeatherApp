991d7792893b80171f50c8bed4d8bbc0511c1586
abdulrazzaaq <shaik.11715485@lpu.in>
2023-10-06 19:50:15 +0530

    Updated the activity and material gradle version.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,13 +41,13 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("io.insert-koin:koin-android:3.5.0")
     implementation ("io.insert-koin:koin-core:3.5.0")
     implementation ("io.insert-koin:koin-annotations:1.3.0")
     ksp ("io.insert-koin:koin-ksp-compiler:1.3.0")
-    implementation("androidx.activity:activity-ktx:1.7.2")
+    implementation("androidx.activity:activity-ktx:1.8.0")
 
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
